94fda334acd4d96170812697cc74b498b33e0da5
ywdblog <ywdblog@gmail.com>
2019-03-23 22:13:17 +0800

    重大更新，操作更简单，如果运行有问题，欢迎@我，或者使用老版本（v2018-12 tag）

--- config.php
+++
@@ -1,9 +0,0 @@
-### aly
-
-LTAIkLV6coSSKklZ
-YEGDVHQV4oBC6AGQM9BWaHStUtNE5M
-
-
-define("txyaccessKeyId", "AKIDwlPr7DUpLgpZBb4tlT0MWUHtIVXOJwxm");
-define("txyaccessSecrec", "mMkxzoTxOirrfJlFYfbS7g7792jEi5GG");
-
