fe0e67b1ac100cf1d768792512e13103e7ac220d
ywdblog <ywdblog@gmail.com>
2019-06-10 13:51:10 +0800

    fix #35

--- php-version/alydns.php
+++ php-version/alydns.php
@@ -1,5 +1,4 @@
 <?php
-
 date_default_timezone_set("GMT");
 
 /*
@@ -51,10 +50,10 @@ if (count($argv) < 7) {
     echo "参数有误\n";
     exit;
 }
-echo $argv[1] . "-" . $argv[2] . "-" . $argv[3] . "-" . $argv[4] . "-" . $argv[5] . "-" . $argv[6] . "\n";
+echo $argv[1]."-".$argv[2]."-".$argv[3]."-".$argv[4]."-".$argv[5]."-".$argv[6]."\n";
 
 $domainarray = AliDns::getDomain($argv[2]);
-$selfdomain = ($domainarray[0] == "") ? $argv[3] : $argv[3] . "." . $domainarray[0];
+$selfdomain  = ($domainarray[0] == "") ? $argv[3] : $argv[3].".".$domainarray[0];
 
 $obj = new AliDns($argv[5], $argv[6], $domainarray[1]);
 
@@ -67,7 +66,7 @@ switch ($argv[1]) {
                 if ($v["RR"] == $selfdomain) {
                     $data = $obj->DeleteDomainRecord($v["RecordId"]);
                     if ($data["httpcode"] != 200) {
-                        echo "aly dns 域名删除失败-" . $data["Code"] . ":" . $data["Message"];
+                        echo "aly dns 域名删除失败-".$data["Code"].":".$data["Message"];
                         exit;
                     }
                 }
@@ -79,7 +78,7 @@ switch ($argv[1]) {
         $data = $obj->AddDomainRecord("TXT", $selfdomain, $argv[4]);
 
         if ($data["httpcode"] != 200) {
-            echo "aly dns 域名增加失败-" . $data["Code"] . ":" . $data["Message"];
+            echo "aly dns 域名增加失败-".$data["Code"].":".$data["Message"];
             exit;
         }
         break;
@@ -89,23 +88,24 @@ echo "域名 API 调用结束\n";
 
 ############ Class 定义
 
-class AliDns {
-
-    private $accessKeyId = null;
+class AliDns
+{
+    private $accessKeyId  = null;
     private $accessSecrec = null;
-    private $DomainName = null;
+    private $DomainName   = null;
 
-    public function __construct($accessKeyId, $accessSecrec, $domain) {
-        $this->accessKeyId = $accessKeyId;
+    public function __construct($accessKeyId, $accessSecrec, $domain)
+    {
+        $this->accessKeyId  = $accessKeyId;
         $this->accessSecrec = $accessSecrec;
-        $this->DomainName = $domain;
+        $this->DomainName   = $domain;
     }
-
     /*
       根据域名返回主机名和二级域名
      */
 
-    public static function getDomain($domain) {
+    public static function getDomain($domain)
+    {
 
         //https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains	
         //常见根域名
@@ -130,15 +130,15 @@ class AliDns {
         //二级域名
         $seconddomain = "";
         //子域名
-        $selfdomain = "";
+        $selfdomain   = "";
         //根域名
-        $rootdomain = "";
+        $rootdomain   = "";
         foreach ($arr as $k => $v) {
             $pos = stripos($domain, $v);
             if ($pos) {
-                $rootdomain = substr($domain, $pos);
-                $s = explode(".", substr($domain, 0, $pos));
-                $seconddomain = $s[count($s) - 1] . $rootdomain;
+                $rootdomain   = substr($domain, $pos);
+                $s            = explode(".", substr($domain, 0, $pos));
+                $seconddomain = $s[count($s) - 1].$rootdomain;
                 for ($i = 0; $i < count($s) - 1; $i++)
                     $selfdomain .= $s[$i];
                 break;
@@ -147,21 +147,23 @@ class AliDns {
         //echo $seconddomain ;exit;
         if ($rootdomain == "") {
             $seconddomain = $domain;
-            $selfdomain = "";
+            $selfdomain   = "";
         }
         return array($selfdomain, $seconddomain);
     }
 
-    public function DescribeDomainRecords() {
+    public function DescribeDomainRecords()
+    {
         $requestParams = array(
             "Action" => "DescribeDomainRecords"
         );
-        $val = $this->send($requestParams);
+        $val           = $this->send($requestParams);
 
         return $this->out($val);
     }
 
-    public function UpdateDomainRecord($id, $type, $rr, $value) {
+    public function UpdateDomainRecord($id, $type, $rr, $value)
+    {
         $requestParams = array(
             "Action" => "UpdateDomainRecord",
             "RecordId" => $id,
@@ -169,20 +171,22 @@ class AliDns {
             "Type" => $type,
             "Value" => $value,
         );
-        $val = $this->send($requestParams);
+        $val           = $this->send($requestParams);
         return $this->out($val);
     }
 
-    public function DeleteDomainRecord($id) {
+    public function DeleteDomainRecord($id)
+    {
         $requestParams = array(
             "Action" => "DeleteDomainRecord",
             "RecordId" => $id,
         );
-        $val = $this->send($requestParams);
+        $val           = $this->send($requestParams);
         return $this->out($val);
     }
 
-    public function AddDomainRecord($type, $rr, $value) {
+    public function AddDomainRecord($type, $rr, $value)
+    {
 
         $requestParams = array(
             "Action" => "AddDomainRecord",
@@ -190,11 +194,12 @@ class AliDns {
             "Type" => $type,
             "Value" => $value,
         );
-        $val = $this->send($requestParams);
+        $val           = $this->send($requestParams);
         return $this->out($val);
     }
 
-    private function send($requestParams) {
+    private function send($requestParams)
+    {
         $publicParams = array(
             "DomainName" => $this->DomainName,
             "Format" => "JSON",
@@ -206,31 +211,33 @@ class AliDns {
             "SignatureNonce" => substr(md5(rand(1, 99999999)), rand(1, 9), 14),
         );
 
-        $params = array_merge($publicParams, $requestParams);
+        $params              = array_merge($publicParams, $requestParams);
         $params['Signature'] = $this->sign($params, $this->accessSecrec);
-        $uri = http_build_query($params);
-        $url = 'http://alidns.aliyuncs.com/?' . $uri;
+        $uri                 = http_build_query($params);
+        $url                 = 'http://alidns.aliyuncs.com/?'.$uri;
         return $this->curl($url);
     }
 
-    private function sign($params, $accessSecrec, $method = "GET") {
+    private function sign($params, $accessSecrec, $method = "GET")
+    {
         ksort($params);
-        $stringToSign = strtoupper($method) . '&' . $this->percentEncode('/') . '&';
+        $stringToSign = strtoupper($method).'&'.$this->percentEncode('/').'&';
 
         $tmp = "";
         foreach ($params as $key => $val) {
-            $tmp .= '&' . $this->percentEncode($key) . '=' . $this->percentEncode($val);
+            $tmp .= '&'.$this->percentEncode($key).'='.$this->percentEncode($val);
         }
-        $tmp = trim($tmp, '&');
-        $stringToSign = $stringToSign . $this->percentEncode($tmp);
+        $tmp          = trim($tmp, '&');
+        $stringToSign = $stringToSign.$this->percentEncode($tmp);
 
-        $key = $accessSecrec . '&';
+        $key  = $accessSecrec.'&';
         $hmac = hash_hmac("sha1", $stringToSign, $key, true);
 
         return base64_encode($hmac);
     }
 
-    private function percentEncode($value = null) {
+    private function percentEncode($value = null)
+    {
         $en = urlencode($value);
         $en = str_replace("+", "%20", $en);
         $en = str_replace("*", "%2A", $en);
@@ -238,25 +245,24 @@ class AliDns {
         return $en;
     }
 
-    private function curl($url) {
-        $ch = curl_init();
+    private function curl($url)
+    {
+        $ch     = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         //curl_setopt($ch, CURLOPT_HEADER, 1); 
 //curl_setopt($ch, CURLINFO_HEADER_OUT, true); 
         $result = curl_exec($ch);
-        $info = curl_getinfo($ch);
+        $info   = curl_getinfo($ch);
 
         curl_close($ch);
         return array($info["http_code"], $result);
     }
 
-    private function out($arr) {
-
-        $t = json_decode($arr[1], true);
+    private function out($arr)
+    {
+        $t             = json_decode($arr[1], true);
         $t["httpcode"] = $arr[0];
-
         return $t;
     }
-
 }
