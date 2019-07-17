b2e2cd78493abb51b04291bf8568d43f24b40b67
Hailiang <1181554113@qq.com>
2019-07-17 16:45:17 +0800

    Update godaddydns.py

--- python-version/godaddydns.py
+++ python-version/godaddydns.py
@@ -13,7 +13,7 @@ class GodaddyDns:
     def getDomain(domain):
         domain_parts = domain.split('.')
         if len(domain_parts) > 2:
-            rootdomain = '.'.join(domain_parts[-(2 if domain_parts[-1] in {"co.jp", "com.tw", "net", "com", "com.cn", "org", "cn", "gov", "net.cn", "io", "top", "me", "int", "edu", "link"} else 3): ])
+            rootdomain = '.'.join(domain_parts[-(2 if domain_parts[-1] in {"co.jp", "com.tw", "net", "com", "com.cn", "org", "cn", "gov", "net.cn", "io", "top", "me", "int", "edu", "link","shop"} else 3): ])
             selfdomain = domain.split(rootdomain)[0]
             return (selfdomain[0:len(selfdomain)-1], rootdomain)
         return ("", domain)
